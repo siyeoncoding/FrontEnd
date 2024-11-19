de86faba349891c3c381171dba54263ee58be21c
siyeoncoding <siso7955@gmail.com>
2024-11-19 15:53:29 +0900

    포트폴리오 상세 설명 기술 및 디자인, 확대 상세보기 기능 추가.

--- src/utils/Portfolio.jsx
+++ src/utils/Portfolio.jsx
@@ -2,53 +2,91 @@ import React, { useState } from "react";
 import '../assets/scss/_portfolio.scss';
 import project_1 from "../assets/img/img1.jpg";
 import project_2 from "../assets/img/img2.jpg";
+import project_3 from "../assets/img/img3.jpg";
+import project_4 from "../assets/img/img4.jpg";
+import project_5 from "../assets/img/img5.jpg";
 
-const PORTFOLIO_INTRO = [
-    {
-        id: "intro_title",
-        p: '제 포트폴리오에 오신 것을 환영합니다!',
-        strong: '“여기는 제가 진행한 프로젝트와 경험을 소개하는 공간입니다.”',
-    },
-    {
-        id: "intro_description",
-        p: '저는 다양한 기술과 도구를 활용하여 여러 프로젝트를 성공적으로 수행해왔습니다. 이러한 경험을 통해 문제 해결 능력과 실무 기술을 발전시켰습니다.',
-        strong: '“이 포트폴리오를 통해 저의 성장 과정을 확인해 주세요.”',
-    }
-];
 
+import project_6 from "../assets/img/img6.png";
+import project_7 from "../assets/img/img7.png";
+import project_8 from "../assets/img/img8.png";
+import project_9 from "../assets/img/img9.png";
 const PROJECTS = [
     {
         id: "project_1",
         title: '의류 쇼핑몰',
-        description: '본 프로젝트는 의류 쇼핑몰 웹사이트입니다. 관리자와 사용자의 계정을 나눠 관리자는 상품등록 사용자는 물건 구매가 가능합니다.' +
-            '사용한 기술은 JAVA, JS, JDBC, MYSQL, Spring Boot입니다. ',
-        image: project_1,
+        images: [project_1, project_3, project_4, project_5],
+        descriptions: [
+            '스프링부트를 사용한 의류 쇼핑몰입니다.',
+            '관리자 계정으로 상품을 등록합니다.',
+            '유저 계정으로 상품 담기 및 구매가 가능합니다.',
+            ' 사용한 기술 스택 입니다. JAVA, JS, JDBC, MYSQL, Spring Boot입니다.'
+        ],
     },
     {
         id: "project_2",
         title: '날씨 분석 및 옷차림 추천 웹사이트',
-        description: '본 프로젝트는 Api를 활용하여 전국의 날씨정보 및 옷차림 추천을 제공하는 웹사이트입니다. 전국별 날씨와 자신이 거주중인 지역의 날씨를 파악하고 ' +
-            '적절한 옷차림을 추천받을 수 있습니다. 사용한 기술은 JAVA, JS, JDBC, MYSQL, Spring Boot 입니다.',
-        image: project_2,
+        images: [project_2, project_6, project_7, project_8, project_9, project_5],
+        descriptions: ['전국 날씨, 회원 지역 날씨 및 옷차림 추천 사이트입니다.',
+            '회원은 자신의 지역을 등록하여 거주지 날씨를 확인 할 수 있습니다.',
+            '전국의 날씨를 한번에 확인이 가능합니다.',
+            '회원 정보를 통해 자세한 날씨 정보 확인이 가능합니다.',
+            '날씨에 알맞은 옷차림을 추천받을 수 있습니다.',
+            '날씨 api를 활용한 기술 스택입니다.  JAVA, JS, JDBC, MYSQL, Spring Boot입니다.'],
     },
 ];
 
-const Modal = ({ title, description, image, onClose }) => {
+
+const ExpandedImageModal = ({ image, onClose }) => {
+    return (
+        <div className="expanded-image-modal">
+            <div className="modal-overlay" onClick={onClose}></div>
+            <div className="modal-content">
+                <img src={image} alt="확대된 이미지" className="expanded-image" />
+                <button className="close-button" onClick={onClose}>닫기</button>
+            </div>
+        </div>
+    );
+};
+
+const Modal = ({ project, onClose }) => {
+    const [index, setIndex] = useState(0);
+    const [expandedImage, setExpandedImage] = useState(null);
+
+    const handleNext = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % project.images.length);
+    };
+
+    const handleImageClick = () => {
+        setExpandedImage(project.images[index]);
+    };
+
+    const closeExpandedImage = () => {
+        setExpandedImage(null);
+    };
+
     return (
         <div className="modal-overlay">
             <div className="modal">
                 <div className="modal-content">
-                    <div className="modal-image">
-                        <img src={image} alt={title} />
+                    <div className="modal-image" onClick={handleImageClick}>
+                        <img src={project.images[index]} alt={project.title} />
                     </div>
                     <div className="modal-description">
-                        <h3>{title}</h3>
-                        <p>{description}</p>
+                        <h3>{project.title}</h3>
+                        <p>{project.descriptions[index]}</p>
                         <div className="modal-button-container">
+                            <button onClick={handleNext}>다음</button>
                             <button onClick={onClose}>닫기</button>
                         </div>
                     </div>
                 </div>
+                {expandedImage && (
+                    <ExpandedImageModal
+                        image={expandedImage}
+                        onClose={closeExpandedImage}
+                    />
+                )}
             </div>
         </div>
     );
@@ -68,43 +106,29 @@ const Portfolio = () => {
     return (
         <div>
             <section id="portfolio">
-                {/* 포트폴리오 소개 섹션 */}
-                <div className="portfolio-intro">
-                    {PORTFOLIO_INTRO.map(item => (
-                        <div key={item.id}>
-                            <p>{item.p}</p>
-                            <strong>{item.strong}</strong>
-                        </div>
-                    ))}
-                </div>
-
-                {/* 프로젝트 섹션 */}
                 <h2 className="project-title">나의 프로젝트</h2>
                 <p className="click-instruction">(이미지를 클릭해주세요.)</p>
                 <div className="project-container">
-                    {PROJECTS.map(item => (
+                    {PROJECTS.map((item) => (
                         <div key={item.id} className="project">
-                            <div className="project-description" onClick={() => handleImageClick(item)}>
+                            <div
+                                className="project-description"
+                                onClick={() => handleImageClick(item)}
+                            >
                                 <h3>{item.title}</h3>
                             </div>
-                            {item.image && (
-                                <div className="image" onClick={() => handleImageClick(item)}>
-                                    <img
-                                        src={item.image}
-                                        alt={item.title}
-                                        className="project-image"
-                                    />
-                                </div>
-                            )}
+                            <div
+                                className="image"
+                                onClick={() => handleImageClick(item)}
+                            >
+                                <img src={item.images[0]} alt={item.title} className="project-image" />
+                            </div>
                         </div>
                     ))}
                 </div>
-
                 {modalData && (
                     <Modal
-                        title={modalData.title}
-                        description={modalData.description}
-                        image={modalData.image}
+                        project={modalData}
                         onClose={handleCloseModal}
                     />
                 )}
